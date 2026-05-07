ee74f557ef301721eee8c8928351169f4deb1386
gamefreak431 <mcleish.john@gmail.com>
2026-05-07 21:23:08 +1000

    Added API key and check if it exits

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+try:
+    api_key = os.environ.get("GEMINI_API_KEY")
+except KeyError:
+    raise KeyError("GEMINI_API_KEY not found in environment variables.")
+
+
+
 def main():
     print("Hello from bootdev-ai-agent-python!")
 
