94981df16373c738e894c9e29522992ba42d6c7c
gamefreak431 <mcleish.john@gmail.com>
2026-05-07 21:44:39 +1000

    MVP, script can call Gemini API, get a responce and print to stdout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from google import genai
 
 load_dotenv()
 
@@ -8,10 +9,17 @@ try:
 except KeyError:
     raise KeyError("GEMINI_API_KEY not found in environment variables.")
 
+client = genai.Client(api_key=api_key)
+gemini_model = "gemini-2.5-flash"
+test_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 
+response = client.models.generate_content(
+    model = gemini_model, contents = test_prompt
+)
 
 def main():
     print("Hello from bootdev-ai-agent-python!")
+    print(response.text)
 
 
 if __name__ == "__main__":
