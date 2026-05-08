a3db28c35dd1ae5816cd6e6416a261c4f906d777
gamefreak431 <mcleish.john@gmail.com>
2026-05-08 21:56:58 +1000

    User can now enter their own prompt, done by calling the script with the prompt in the terminal

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
 load_dotenv()
+parser = argparse.ArgumentParser()
+parser.add_argument("user_prompt", help="The prompt to send to the Gemini API")
+args = parser.parse_args()
+
+prompt = args.user_prompt
+test_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 
 try:
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -11,17 +18,16 @@ except KeyError:
 
 client = genai.Client(api_key=api_key)
 gemini_model = "gemini-2.5-flash"
-test_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 
 response = client.models.generate_content(
-    model = gemini_model, contents = test_prompt
+    model = gemini_model, contents = prompt
 )
 
 if response.usage_metadata is None:
     raise RuntimeError("Response does not contain usage metadata. Likely a failed API call, try running the script again.")
 
 def main():
-    print(f"User prompt: {test_prompt}")
+    print(f"User prompt: {prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response: \n{response.text}")
