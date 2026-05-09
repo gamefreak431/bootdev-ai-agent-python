9236e7256a317fe237577d08b9854543fbbde973
gamefreak431 <mcleish.john@gmail.com>
2026-05-09 18:46:48 +1000

    Refactor to move API call into it's own function.

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+def generate_content(client, messages, gemini_model):
+    response = client.models.generate_content(
+        model = gemini_model, contents = messages
+    )
+    if response.usage_metadata is None:
+        raise RuntimeError("Response does not contain usage metadata. Likely a failed API call, try running the script again.")
+    return response
+
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -18,12 +26,7 @@ def main():
     prompt = args.user_prompt
     messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
 
-    response = client.models.generate_content(
-        model = gemini_model, contents = messages
-    )
-    if response.usage_metadata is None:
-        raise RuntimeError("Response does not contain usage metadata. Likely a failed API call, try running the script again.")
-
+    response = generate_content(client, messages, gemini_model)
     print(f"User prompt: {prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
