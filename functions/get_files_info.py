ed78391ce0eced159d9ce8bf85a3ad56017ae6c0
gamefreak431 <mcleish.john@gmail.com>
2026-05-13 21:33:06 +1000

    Updated get_files_info to return list of files and directories within a path. Fixed bug where subdirectories were returning as not a dir. Updated test script per new requirements

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,17 +1,25 @@
 import os
 
-def get_files_info(working_directory: str, directory: str = ".") -> str:
-    if not os.path.isdir(directory):
-        return f'Error: "{directory}" is not a directory'
-    
+def get_files_info(working_directory: str, directory: str = ".") -> str:  
     working_directory = os.path.abspath(working_directory)
     target_dir = os.path.normpath(os.path.join(working_directory, directory))
     try:
         valid_target_dir = os.path.commonpath([working_directory, target_dir]) == working_directory
     except ValueError as e:
-        return f'Error: {str(e)}'
-
-
+        return f'Error: valid_target_dir - {str(e)}'
     if not valid_target_dir:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    return f'Success: "{directory}" is within the working directory'
+    if not os.path.isdir(target_dir):
+        return f'Error: "{target_dir}" is not a directory'
+    
+    dir_info = []
+    for dir in os.listdir(target_dir):
+        dir_path = os.path.join(target_dir, dir)
+        try:
+            size = os.path.getsize(dir_path)
+        except OSError as e:
+            return f'Error: size - "{dir_path}": {str(e)}'
+        is_dir = os.path.isdir(dir_path)
+        dir_info.append(f'- {dir}: file_size={size} bytes, is_dir={is_dir}')
+    
+    return '\n'.join(dir_info)
--- test_get_files_info.py
+++ test_get_files_info.py
@@ -3,14 +3,14 @@ from functions.get_files_info import get_files_info
 def main():
     tests = [
         ("calculator", "."),
+        ("calculator", "pkg"),
         ("calculator", "/bin"),
         ("calculator", "../"),
-        ("calculator", "main.py")
     ]
 
     for working_directory, directory in tests:
         result = get_files_info(working_directory, directory)
-        print(result)
+        print(f"Result for '{directory}' directory:\n{result}")
 
 
 if __name__ == "__main__":
