c02cb04f74d7ce9e61d3c3dd7e605df9c6133c64
gamefreak431 <mcleish.john@gmail.com>
2026-05-18 22:09:55 +1000

    Updated config to remove MAX_CHARS as it was creating a circular dependency error. Added constants.py and moved variable there

--- config.py
+++ config.py
@@ -1,10 +1,16 @@
 from google.genai import types
 from functions.get_files_info import schema_get_files_info
+from functions.get_file_content import schema_get_file_content
+from functions.write_file import schema_write_file
+from functions.run_python_file import schema_run_python_file
 
 
-MAX_CHARS = 10000
 available_functions = types.Tool(
-    function_declarations=[schema_get_files_info],
+    function_declarations=[
+        schema_get_files_info,
+        schema_get_file_content,
+        schema_write_file,
+        schema_run_python_file],
 )
 system_prompt = """
 You are a helpful AI coding agent.
@@ -12,6 +18,9 @@ You are a helpful AI coding agent.
 When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
 
 - List files and directories
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
